ee02020e00de214b2437cb9ea28ecfd41fd79e19
Tim Ostermann <tim.ostermann@i22.de>
2024-01-20 16:30:09 +0100

    config: add react eslint setup

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -14,5 +14,59 @@ export default {
       rules: {
       },
     },
+    {
+      files: ["**/*.tsx", "**/*.ts"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        project: "./tsconfig.json",
+      },
+      extends: [
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+      ],
+      settings: {
+        react: {
+          version: "detect",
+        },
+        "import/resolver": {
+          typescript: {
+            project: "./tsconfig.json",
+          },
+        },
+      },
+      plugins: ["import"],
+      rules: {
+        "no-undef": "off",
+        "react/prop-types": "off",
+        "no-anonymous-exports-page-templates": "warn",
+        "limited-exports-page-templates": "warn",
+        "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        "@typescript-eslint/consistent-type-exports": "error",
+        "@typescript-eslint/consistent-type-imports": "error",
+        "@typescript-eslint/no-floating-promises": "error",
+        quotes: [
+          "error",
+          "double",
+          { avoidEscape: true, allowTemplateLiterals: false },
+        ],
+        "import/order": [
+          "error",
+          {
+            groups: [
+              "builtin",
+              "external",
+              "internal",
+              "parent",
+              "sibling",
+              "index",
+            ],
+            "newlines-between": "always",
+          },
+        ],
+      },
+    },
   ],
 }
