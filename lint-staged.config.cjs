60a79a8abaa8d0f4479f1e12f510e53d94c1a92a
Tim Ostermann <tim.ostermann@i22.de>
2024-01-20 16:35:07 +0100

    config: setup husky hook

---
+++ lint-staged.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  "*.{json,js,jsx,ts,tsx}": "npx prettier --write",
+  "*.{json,js,jsx,ts,tsx,astro}": "npm run check",
+  "*.{js,jsx,ts,tsx}": "npm run eslint -- --fix",
+  "*.{ts,tsx}": () => {
+    return "tsc --noEmit";
+  },
+};
