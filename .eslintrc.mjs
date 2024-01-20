7ca7a997ff94f1a428b998e7568333d0aa952135
Tim Ostermann <tim.ostermann@i22.de>
2024-01-20 16:12:28 +0100

    config: add astro eslint

---
+++ .eslintrc.mjs
@@ -0,0 +1,18 @@
+export default {
+  extends: [
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended"
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+      },
+    },
+  ],
+}
