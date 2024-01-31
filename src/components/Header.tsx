1a8d4e094165fe324e5fad7f529ecaec1533a153
Tim Ostermann <tim.ostermann@i22.de>
2024-01-31 21:55:20 +0100

    feat: implement icons in header nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import cn from "classnames";
 import {
   useState,
   type ComponentPropsWithoutRef,
-  type ReactNode,
   useRef,
   useEffect,
+  Fragment,
 } from "react";
 import { Container, ContainerMargin } from "./Container";
 
@@ -14,7 +14,7 @@ type NavItem = {
   subItems?: Array<{
     label: string;
     href: string;
-    icon?: ReactNode;
+    icon?: string;
   }>;
 };
 
@@ -73,14 +73,19 @@ export const Header = ({
                 >
                   <path d="M16.59 8.59L12 13.17L7.41 8.59L6 10L12 16L18 10L16.59 8.59Z" />
                 </svg>
-                <ul className="invisible absolute left-0 top-full w-56 -translate-y-4 rounded-lg border border-gray-100 bg-white py-4 opacity-0 shadow-md transition-[opacity,_transform] group-focus-within:visible group-focus-within:translate-y-0 group-focus-within:opacity-100 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
+                <ul className="invisible absolute left-0 top-full flex w-56 -translate-y-4 flex-col gap-2 rounded-lg border border-gray-100 bg-white py-4 opacity-0 shadow-md transition-[opacity,_transform] group-focus-within:visible group-focus-within:translate-y-0 group-focus-within:opacity-100 group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
                   {item.subItems.map((subItem) => (
                     <li key={subItem.label}>
                       <a
                         href={subItem.href}
-                        className="flex items-center gap-2 px-4 py-2 text-gray-900 hover:bg-gray-100"
+                        className="flex items-center gap-4 px-4 py-2 text-gray-900 hover:bg-gray-100"
                       >
-                        {subItem.icon}
+                        {subItem.icon && (
+                          <span
+                            className="text-primary-700"
+                            dangerouslySetInnerHTML={{ __html: subItem.icon }}
+                          />
+                        )}
                         {subItem.label}
                       </a>
                     </li>
@@ -185,13 +190,18 @@ const MobileNavItem = ({ label, href, subItems }: NavItem) => {
             {subItems?.map((subItem) => (
               <li
                 key={subItem.label}
-                className="flex flex-col px-4 py-2 first:mt-4 last:mb-4"
+                className="flex flex-col gap-2 px-4 first:mt-4 last:mb-4"
               >
                 <a
                   href={subItem.href}
-                  className="flex w-full items-center gap-2 text-gray-900 hover:bg-gray-100"
+                  className="flex w-full items-center gap-4 py-2 text-gray-900 hover:bg-gray-100"
                 >
-                  {subItem.icon}
+                  {subItem.icon && (
+                    <span
+                      className="text-primary-700"
+                      dangerouslySetInnerHTML={{ __html: subItem.icon }}
+                    />
+                  )}
                   {subItem.label}
                 </a>
               </li>
