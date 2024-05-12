f0439595486bd8c3b7169d256125aa3a7affee08
Tim Ostermann <tim.ostermann@i22.de>
2024-05-12 16:58:08 +0200

    feat: add global focus offset style

--- src/components/LanguageSwitch.tsx
+++ src/components/LanguageSwitch.tsx
@@ -10,20 +10,20 @@ export const LanguageSwitch = ({
   const router = useRouter();
 
   return (
-    <ul className={cn("flex gap-1", className)} {...props}>
+    <ul className={cn("flex gap-1.5", className)} {...props}>
       {router.locales?.map((locale) => (
         <li key={locale}>
           <Link
             href=""
             locale={locale}
             className={cn(
-              "p-0.5 text-sm font-light opacity-50 transition-colors hover:opacity-100 focus-visible:opacity-100",
+              "text-sm font-light opacity-50 transition-colors hover:opacity-100 focus-visible:opacity-100",
               {
-                "font-medium opacity-100": locale === router.locale,
+                "font-medium !opacity-100": locale === router.locale,
               },
             )}
           >
-            {locale.toUpperCase()}
+            {locale}
           </Link>
         </li>
       ))}
