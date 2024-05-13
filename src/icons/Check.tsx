e89b40cd52fb67bc036bdb1e6e990440acd11702
Tim Ostermann <tim.ostermann@i22.de>
2024-05-13 20:57:04 +0200

    feat: add check icon

---
+++ src/icons/Check.tsx
@@ -0,0 +1,25 @@
+import { useSanitizedId } from "@/lib/sanitizeString";
+import type { SVGIconProps } from "@/types";
+
+export const Certificate = ({ title, ...props }: SVGIconProps) => {
+  const id = useSanitizedId();
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      aria-hidden={!title}
+      aria-labelledby={title ? id : undefined}
+      {...props}
+    >
+      {title && <title id={id}>{title}</title>}
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m4.5 12.75 6 6 9-13.5"
+      />
+    </svg>
+  );
+};
