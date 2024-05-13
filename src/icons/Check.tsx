4827dd0460a21addb7c480e0f1165fe9209e75a9
Tim Ostermann <tim.ostermann@i22.de>
2024-05-13 21:22:24 +0200

    feat: replace check icon with custom designed one

--- src/icons/Check.tsx
+++ src/icons/Check.tsx
@@ -5,20 +5,19 @@ export const Check = ({ title, ...props }: SVGIconProps) => {
   const id = useSanitizedId();
   return (
     <svg
-      viewBox="0 0 24 24"
+      viewBox="0 0 13 11"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      strokeWidth={1.5}
-      stroke="currentColor"
       aria-hidden={!title}
       aria-labelledby={title ? id : undefined}
       {...props}
     >
       {title && <title id={id}>{title}</title>}
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="m4.5 12.75 6 6 9-13.5"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11.098 0.390159L3.93797 7.30016L2.03797 5.27016C1.68797 4.94016 1.13797 4.92016 0.737968 5.20016C0.347968 5.49016 0.237968 6.00016 0.477968 6.41016L2.72797 10.0702C2.94797 10.4102 3.32797 10.6202 3.75797 10.6202C4.16797 10.6202 4.55797 10.4102 4.77797 10.0702C5.13797 9.60016 12.008 1.41016 12.008 1.41016C12.908 0.490159 11.818 -0.31984 11.098 0.38016V0.390159Z"
+        fill="currentColor"
       />
     </svg>
   );
