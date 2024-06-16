8da676c44b1f4435da38af3122bb7eef7b1300ef
Tim Ostermann <tim.ostermann@i22.de>
2024-06-16 18:11:50 +0200

    feat: add dgv logo to platzreife page

--- src/pages/golfkurse/platzreife.tsx
+++ src/pages/golfkurse/platzreife.tsx
@@ -1,9 +1,11 @@
 import { type NextPage } from "next";
+import Image from "next/image";
 import { Meta } from "@/components/Meta";
 import { Container, ContainerMargin } from "@/components/Container";
 import { Headline, HeadlineTag, HeadlineVariant } from "@/components/Headline";
 import { platzreifeFaq } from "@/data/faq.data";
 import { Accordion } from "@/components/Accordion";
+import dgv from "~/public/images/dgv.webp";
 
 const Platzreife: NextPage = () => (
   <>
@@ -14,32 +16,39 @@ const Platzreife: NextPage = () => (
     <Container
       aria-labelledby="platzreife"
       className="bg-gray-50"
-      innerClassName="flex flex-col py-20 lg:py-28"
+      innerClassName="flex flex-col max-lg:items-center lg:flex-row lg:justify-between py-20 lg:py-28"
       margin={ContainerMargin.NONE}
     >
-      <Headline
-        tag={HeadlineTag.H1}
-        variant={HeadlineVariant.PRIMARY}
-        id="platzreife"
-      >
-        DGV-Platzreifekurs
-      </Headline>
-      <p aria-hidden className="-order-1 mb-3 text-primary-600">
-        Golfkurse
-      </p>
-      <p className="my-10 max-w-[768px] text-pretty text-gray-500 lg:text-xl">
-        Ihr Entschluss steht fest, jetzt fehlt nur noch die Anmeldung.
-        <br />
-        <br />
-        Dann sind Sie hier bei uns genau richtig! Basierend auf Ihren
-        individuellen Fähigkeiten begleiten wir Sie zur Platzreife. In kleinen
-        Gruppen wird intensiv geübt, mit Spaß und Freude. Mit dem notwendigen
-        Ernst bei der Sache und in lockerer Atmosphäre lässt sich der Kurs
-        mühelos absolvieren.
-        <br />
-        <br />
-        Auf geht&apos;s, wir freuen uns auf Sie.
-      </p>
+      <div className="flex flex-col">
+        <Headline
+          tag={HeadlineTag.H1}
+          variant={HeadlineVariant.PRIMARY}
+          id="platzreife"
+        >
+          DGV-Platzreifekurs
+        </Headline>
+        <p aria-hidden className="-order-1 mb-3 text-primary-600">
+          Golfkurse
+        </p>
+        <p className="my-10 max-w-[768px] text-pretty text-gray-500 lg:text-xl">
+          Ihr Entschluss steht fest, jetzt fehlt nur noch die Anmeldung.
+          <br />
+          <br />
+          Dann sind Sie hier bei uns genau richtig! Basierend auf Ihren
+          individuellen Fähigkeiten begleiten wir Sie zur Platzreife. In kleinen
+          Gruppen wird intensiv geübt, mit Spaß und Freude. Mit dem notwendigen
+          Ernst bei der Sache und in lockerer Atmosphäre lässt sich der Kurs
+          mühelos absolvieren.
+          <br />
+          <br />
+          Auf geht&apos;s, wir freuen uns auf Sie.
+        </p>
+      </div>
+      <Image
+        src={dgv}
+        alt="Deutscher Golf Verband Logo"
+        className="size-[200px] rounded-full"
+      />
     </Container>
     <Container
       aria-labelledby="faq"
