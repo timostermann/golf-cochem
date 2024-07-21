04a7574dd096b46e0efd499dbe45100b88f904c2
Tim Ostermann <tim.ostermann@i22.de>
2024-07-21 21:24:38 +0200

    feat: add empty impressum page

---
+++ src/pages/impressum.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from "next";
+import { Meta } from "@/components/Meta";
+import { Container } from "@/components/Container";
+
+const Impressum: NextPage = () => (
+  <>
+    <Meta
+      title="Impressum"
+      description="Impressum der Golfclub Cochem/Mosel Webseite"
+      robots="noindex, nofollow"
+    />
+    <Container>TODO Impressum</Container>
+  </>
+);
+
+export default Impressum;
