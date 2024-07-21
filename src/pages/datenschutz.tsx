acce792ff3277b613c054291b2bcd7e86d05ee66
Tim Ostermann <tim.ostermann@i22.de>
2024-07-21 21:25:03 +0200

    feat: add empty datenschutz page

---
+++ src/pages/datenschutz.tsx
@@ -0,0 +1,16 @@
+import { type NextPage } from "next";
+import { Meta } from "@/components/Meta";
+import { Container } from "@/components/Container";
+
+const Datenschutz: NextPage = () => (
+  <>
+    <Meta
+      title="Datenschutz"
+      description="Datenschutzerklärung der Golfclub Cochem/Mosel Webseite"
+      robots="noindex, nofollow"
+    />
+    <Container>TODO Datenschutz</Container>
+  </>
+);
+
+export default Datenschutz;
