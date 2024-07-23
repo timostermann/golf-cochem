f3823cb467bfbc1b7ca81192b9b80b9739f9ff15
Tim Ostermann <tim.ostermann@i22.de>
2024-07-23 20:35:15 +0200

    feat: add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,50 @@
+import React, { type ComponentPropsWithoutRef, forwardRef, useId } from "react";
+import cn from "classnames";
+
+type InputProps = ComponentPropsWithoutRef<"input"> & {
+  label: string;
+  error?: string;
+  touched?: boolean;
+};
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, touched, className, required, ...props }, ref) => {
+    const id = useId();
+    const showError = touched && error;
+
+    return (
+      <div className="mb-4">
+        <label
+          htmlFor={id}
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
+          {label} {required && <span aria-hidden>*</span>}
+        </label>
+        <input
+          ref={ref}
+          id={id}
+          className={cn(
+            "w-full rounded-lg border border-gray-300 px-3 py-2 text-gray-700 focus:border-primary-500 focus:outline-none focus:ring-2",
+            { "border-red-500": showError },
+            className,
+          )}
+          aria-invalid={showError ? "true" : "false"}
+          aria-describedby={showError ? `${id}-error` : undefined}
+          required={required}
+          {...props}
+        />
+        {showError && (
+          <p
+            id={`${id}-error`}
+            aria-live="polite"
+            className="relative text-xs text-red-500"
+          >
+            <span className="absolute ml-1 mt-0.5">{error}</span>
+          </p>
+        )}
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
