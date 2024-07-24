6e51c3aed63006fcb1b4e06b76e10b10484e4a64
Pascal Friedrich <pascal.friedrich@me.com>
2024-07-24 22:06:18 +0200

    feat: add blogpost api to details page

--- src/pages/clubleben/news/[slug].tsx
+++ src/pages/clubleben/news/[slug].tsx
@@ -6,6 +6,7 @@ import { Container, ContainerSize } from "@/components/Container";
 import { Headline, HeadlineTag, HeadlineVariant } from "@/components/Headline";
 import { Meta } from "@/components/Meta";
 import { Markdown } from "@/components/Markdown";
+import { fetchApi } from "@/lib/strapi";
 
 type NewsArticleProps = {
   article: Blogpost;
@@ -94,170 +95,42 @@ export default NewsArticlePage;
 export const getStaticProps: GetStaticProps<NewsArticleProps> = async ({
   params,
 }) => {
-  //   const blogPostData = await fetchApi<Blogpost[]>({
-  //     endpoint:
-  //       "/blogposts?populate=titleimage&populate=category&populate=author",
-  //     query: {
-  //       "sort[0]": "createdAt:desc",
-  //     },
-  //   });
-
   if (!params?.slug || Array.isArray(params.slug)) {
     return {
       notFound: true,
     };
   }
 
-  const dummyData: Blogpost = {
-    id: "1",
-    title: "Glücklich und zufrieden! – Kanada brachte neue Erkenntnisse",
-    teaser:
-      "Bundestrainer zufrieden und mit einem klaren Plan für das Wintertraining.",
-    category: {
-      id: "1",
-      name: "Club",
+  const blogPostData = await fetchApi<Blogpost[]>({
+    endpoint:
+      "/blogposts?populate=titleimage&populate=category&populate=author",
+    query: {
+      "filters[slug][$eq]": params.slug,
     },
-    author: {
-      id: 1,
-      name: "Ulrike Rummel",
-      picture: {
-        url: "https://picsum.photos/800/400",
-        width: 800,
-        height: 400,
-        alternativeText: "",
-        caption: "",
-        ext: ".jpeg",
-        formats: {},
-        id: 1,
-        mime: "image/jpeg",
-        name: "ulrike-rummel",
-        previewUrl: null,
-        size: 0.09,
-      },
-    },
-    slug: params.slug,
-    content:
-      "# Sub Ilion resonabat et illa resistunt impetus\n" +
-      "\n" +
-      "## Adspexere perpetiar manus in arces oblitus virum\n" +
-      "\n" +
-      "Lorem markdownum gemma sors dextra habe prosit stellatus, attonitaeque pugnae.\n" +
-      "Oraque quis, sic Danai uterque et falsa, saepe commissaque tamen simplex.\n" +
-      "Stupuit est sonus sum forma induere expetit; nostro mihi ignarus, fallique\n" +
-      "prius!\n" +
-      "\n" +
-      "Auxilioque quod, mox mollitaque quod est haurit ponit ipse sulphure, hiemisque.\n" +
-      "Tremulis aequoris. Fuerat nata dixit ne induruit Agaue res frater fortissima\n" +
-      "aura Aesonides recepit **nec nectareis viro**, ait curae.\n" +
-      "\n" +
-      "### Nati Caeni superamur pereat edidicisse\n" +
-      "\n" +
-      "Medio possem, non petere sed multorum male virgo ab gelidum nunc memorare?\n" +
-      "Victoria modo; nata *iurant deficit* de utere parvum fefellimus Bubastis\n" +
-      "draconem iam ipsam nisi. Illi **nascentur** ingenium dura, flammas, sed erit\n" +
-      "adii medulla exercita: eodem?\n" +
-      "\n" +
-      "#### Ulla rura fuit arces valuere missus recusat\n" +
-      "\n" +
-      "*Caelum* invisumque mens [galeae](http://www.invitum-ecce.org/osculareddat.php),\n" +
-      "ei liquidissimus verbis superstitibus iuvenum cum quo flamina Iovis. Spinea\n" +
-      "neque hortaturque, in modo, saxo Ammon, egressus Saturnius, fluviis simul.\n" +
-      "Nascendi torpetis.\n" +
-      "\n" +
-      "![pexels-kindel-media-6572957.jpg](https://golf-cochem.fra1.digitaloceanspaces.com/b4a3c251f5f9058d6f5c2889799765b7.jpg)\n" +
-      "\n" +
-      "#### Ulla rura fuit arces valuere missus recusat\n" +
-      "\n" +
-      "*Caelum* invisumque mens [galeae](http://www.invitum-ecce.org/osculareddat.php),\n" +
-      "ei liquidissimus verbis superstitibus iuvenum cum quo flamina Iovis. Spinea\n" +
-      "neque hortaturque, in modo, saxo Ammon, egressus Saturnius, fluviis simul.\n" +
-      "Nascendi torpetis.\n" +
-      "\n" +
-      "\n" +
-      ">Quo tibia postquam\n" +
-      "teneri ipse forma laniare. Terra umor iterque, dum qui portibus, quem pronus\n" +
-      "deprecor videt est abluere? Sucis alter est: redituraque et sub Lavini sceptrateneri ipse forma laniare. Terra umor iterque, dum qui portibus, quem pronus\n" +
-      "deprecor videt est abluere? Sucis alter est: redituraque et sub Lavini sceptra\n" +
-      "\n" +
-      "Populosque\n" +
-      "tuorum totum capitis sine saxum, visaque more, pertimuitque Iove, quisquis\n" +
-      "frigusque ille visus observo iussit, lenius. Illa quid ab nemorum quotiens. Nec\n" +
-      "quod quendam cum credulus imagine: antris genitor perit per!\n" +
-      "\n" +
-      "## Ultima rerum carinis\n" +
-      "\n" +
-      "Aut te bracchia cuspidis Scylla obstabat ingredior oppidaque genus flectebat\n" +
-      "**feres** navis et cognita falli capiat, removente, neque. Petebar his potenti\n" +
-      "captam illo [iubet](http://septenis-sed.io/ora.html), spectans! Populosque\n" +
-      "tuorum totum capitis sine saxum, visaque more, pertimuitque Iove, quisquis\n" +
-      "frigusque ille visus observo iussit, lenius. Illa quid ab nemorum quotiens. Nec\n" +
-      "quod quendam cum credulus imagine: antris genitor perit per!\n" +
-      "\n" +
-      "    var ssd = prom_dbms_domain;\n" +
-      "    windowsSrgbKilobit += thread(2) + ldap_real;\n" +
-      "    var softwareModemVlb = -4;\n" +
-      "\n" +
-      "Quarum sit manat quam mensae nunc atras medio exitiabile idem aura misit\n" +
-      "Hyleusque aptos, et? Ducentem **tuus** veste grandine orbem. Quo tibia postquam\n" +
-      "teneri ipse forma laniare. Terra umor iterque, dum qui portibus, quem pronus\n" +
-      "deprecor videt est abluere? Sucis alter est: redituraque et sub Lavini sceptra:\n" +
-      "quae huc linguas *procul*; uti salicta siqua *an*.\n" +
-      "\n" +
-      "- Listeneintrag 1\n" +
-      "- Listeneintrag 2\n" +
-      "- Sehr laaaaaaaaaanger laaaaaaaaaanger laaaaaaaaaanger Listeneintrag mit Umbruch\n" +
-      "\n" +
-      "Quarum sit <u>manat</u> quam mensae nunc atras medio exitiabile idem aura misit\n" +
-      "~~Hyleusque~~ aptos, et? Ducentem **tuus** veste grandine orbem. Quo tibia postquam\n" +
-      "teneri ipse forma laniare. Terra umor iterque, dum qui portibus, quem pronus\n" +
-      "deprecor videt est abluere? Sucis alter est: redituraque et sub Lavini sceptra:\n" +
-      "quae huc linguas *procul*; uti salicta siqua *an*.\n" +
-      "\n" +
-      "1. Listeneintrag 1\n" +
-      "2. Listeneintrag 2\n" +
-      "3. Sehr laaaaaaaaaanger laaaaaaaaaanger laaaaaaaaaanger Listeneintrag mit Umbruch\n" +
-      "\n" +
-      "Quarum sit <u>manat</u> quam mensae nunc atras medio exitiabile idem aura misit\n" +
-      "~~Hyleusque~~ aptos, et? Ducentem **tuus** veste grandine orbem. Quo tibia postquam\n" +
-      "teneri ipse forma laniare. Terra umor iterque, dum qui portibus, quem pronus\n" +
-      "deprecor videt est abluere? Sucis alter est: redituraque et sub Lavini sceptra:\n" +
-      "quae huc linguas *procul*; uti salicta siqua *an*.\n" +
-      "\n",
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
-    locale: "de",
-    titleimage: {
-      id: 1,
-      name: "Bild",
-      url: "https://picsum.photos/960/500",
-      width: 960,
-      height: 500,
-      alternativeText: "",
-      caption: "",
-      ext: ".jpeg",
-      formats: {},
-      mime: "image/jpeg",
-      previewUrl: null,
-      size: 0.09,
-    },
-  };
+  });
 
-  return {
-    props: {
-      article: dummyData,
-    },
-  };
+  if (blogPostData[0]) {
+    return {
+      props: {
+        article: blogPostData[0],
+      },
+    };
+  }
+
+  return { notFound: true };
 };
 
 export const getStaticPaths = async () => {
+  const slugsResponse = await fetchApi<{ slug: string }[]>({
+    endpoint: "/blogposts?fields=slug",
+  });
+
+  const paths = slugsResponse.map((slugData) => ({
+    params: { slug: slugData.slug },
+  }));
+
   return {
-    paths: [
-      {
-        params: {
-          slug: "gluecklich-und-zufrieden-kanada-brachte-neue-erkenntnisse",
-        },
-      },
-    ],
+    paths: paths,
     fallback: "blocking",
   };
 };
