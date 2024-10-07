6c27db02a59eee04919787ee9f5a23631d5c1a45
Pascal Friedrich <pascal.friedrich@me.com>
2024-10-07 09:17:26 +0200

    feat: add time to statusCards

--- src/components/StatusCard.tsx
+++ src/components/StatusCard.tsx
@@ -23,18 +23,18 @@ export const StatusCard = ({
   return (
     <div
       className={cn(
-        "flex min-h-40 w-40 flex-col items-center rounded-2xl bg-primary-100 px-1 py-5 text-primary-800 shadow-md sm:w-48 sm:px-3 lg:size-60 lg:p-8",
+        "flex min-h-48 w-48 flex-col items-center rounded-2xl bg-primary-100 px-1 py-5 text-primary-800 shadow-md sm:w-48 sm:px-3 lg:size-60 lg:p-8",
         className,
       )}
       {...props}
     >
       <span className="size-12 lg:size-16">{icon}</span>
-      <p className="mb-5 mt-2 text-center text-base font-bold lg:mb-8 lg:mt-4 lg:text-2xl">
+      <p className="mt-2 text-center text-base font-bold xs:mb-6 lg:mb-8 lg:mt-4 lg:text-2xl">
         {children}
       </p>
       <span
         className={cn(
-          "rounded-2xl px-2 py-1 text-[0.6875rem] sm:px-3 sm:text-xs lg:text-sm",
+          "rounded-2xl px-2 py-1 text-center text-[0.6875rem] sm:px-3 sm:text-xs lg:text-sm",
           {
             "border border-green-700 bg-green-50 text-green-700": open,
             "border border-red-700 bg-red-50 text-red-700": !open,
@@ -42,15 +42,22 @@ export const StatusCard = ({
         )}
       >
         {open ? t("opened") : t("closed")}
-        {until
-          ? ` ${t("until")} ${new Date(until).toLocaleDateString(
-              router.locale,
-              {
-                day: "numeric",
-                month: "numeric",
-              },
-            )}`
-          : ""}
+        {until ? (
+          <>
+            {" "}
+            {t("until")} <br />
+            {new Date(until).toLocaleString(router.locale, {
+              day: "numeric",
+              month: "numeric",
+              year: "numeric",
+              hour: "numeric",
+              minute: "numeric",
+            })}{" "}
+            {t("oClock")}
+          </>
+        ) : (
+          ""
+        )}
       </span>
     </div>
   );
